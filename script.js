2240288849053b7053b9bce4b128b20394ddea98
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-22 13:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function rotateLine(){
+	let rotation=0;
+	const line=document.getElementById("line");
+	function update(){
+		rotation=rotation+5;
+		line.style.transform="rotate(${rotation}deg)";
+	}
+	setInterval(update, 100);
+	
+}
+window.onload=rotateLine;
